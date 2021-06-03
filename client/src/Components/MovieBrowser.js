dad8cb9a75121dcc1d84150e6f3e775d116bf932
zilong-yang <yziLONG1325>
2021-06-02 22:57:42 -0400

    File cleanup + code format
    - removed server/node_modules
    - added .gitignore for /server and root dir to ignore irrelevant folders (caches, IDE settings, etc.)

--- client/src/Components/MovieBrowser.js
+++ client/src/Components/MovieBrowser.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 class MovieBrowser extends React.Component {
-    constructor(){
-        super()
+    constructor(props) {
+        super(props)
 
         // Waiting for API to return
         this.state = {
@@ -10,30 +10,35 @@ class MovieBrowser extends React.Component {
             loading: true
         }
     }
-    // Performin API Call
+
+    // Performing API Call
     async componentDidMount() {
         const api_key = process.env.REACT_APP_TMDB_API_KEY
 
-        const fetch_movies = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=1`)
+        const fetch_movies =
+            await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=en-US&page=1`)
         const movies = await fetch_movies.json()
         console.log(movies)
 
-        this.setState({
-            movies: this.state.movies.concat(movies.results),
-            loading: false
-        },
-        () => {
-            console.log(this.state.movies)
-        })
+        this.setState(
+            {
+                movies: this.state.movies.concat(movies.results),
+                loading: false
+            },
+
+            () => {
+                console.log(this.state.movies)
+            }
+        )
     }
 
     render() {
-        return(
+        return (
             // return is HTML, using {} would allow JS
             // <div> should wrap the return values (there is only a single element)
             <div>
-                {this.state.loading ? 
-                    <p>Loading</p> : 
+                {this.state.loading ?
+                    <p>Loading</p> :
                     <p>Finished</p>}
             </div>
         )
