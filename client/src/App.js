97e66a4dbf328cd29892a5b0dc2b2edf6141145a
Z <zyang.0085@gmail.com>
2021-06-12 15:07:29 -0400

    App.js use hook instead of class
    - state setters are passed into each page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MovieBrowser from './Components/MovieBrowser'
 import TicketSelection from './Components/TicketSelection'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 require('dotenv').config()
 
-class App extends React.Component {
+function App(props) {
 
-    render() {
-        return (
-            <>
-                <Router>
-                    <Switch>
-                        <Route path="/" exact component={MovieBrowser}/>
-                        <Route path="/tickets/:movieID" exact component={TicketSelection}/>
-                    </Switch>
-                </Router>
-            </>
-        )
-    }
+    const [movieName, setMovieName] = useState("");
+    const [movieTime, setMovieTime] = useState("");
+    const [numAdults, setNumAdults] = useState(0);
+    const [numChildren, setNumChildren] = useState(0);
+    const [numSeniors, setNumSeniors] = useState(0);
+    const [seats, setSeats] = useState([]);
+    const [total, setTotal] = useState(0.0);
+
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route path="/" exact render={(props) => (
+                        <MovieBrowser {...props} setters={[setMovieName]}/>
+                    )}/>
+                    <Route path="/tickets/:movieID" exact render={(props) => (
+                        <TicketSelection {...props}
+                            setters={[setMovieTime, setNumAdults, setNumChildren, setNumSeniors]} />
+                    )}/>
+                </Switch>
+            </Router>
+        </>
+    )
 }
 
 export default App;
