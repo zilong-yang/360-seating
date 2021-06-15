24c7acbcbd3ebf597d051e64e2520e726d933784
Rodrigo Moscoso <moscosor@wit.edu>
2021-06-15 19:57:54 -0400

    minor fixes

---
+++ client/src/Components/Seats.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Seats extends Component {
+
+    
+    render() {
+        return (
+            <div>
+                SEATS SELECTION
+            </div>
+        );
+    }
+}
+
+export default Seats;
