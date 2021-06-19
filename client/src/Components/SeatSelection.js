ea87790c119a0302ababb9671b38f3ef26bae23a
Martin Cheung <cheungt1@wit.edu>
2021-06-19 14:14:31 -0400

    show seats from backend and store selected seats on an array

--- client/src/Components/SeatSelection.js
+++ client/src/Components/SeatSelection.js
@@ -2,170 +2,111 @@ import React from 'react';
 import SeatTile from './SeatTile'
 import BackButton from "./BackButton";
 import NextButton from "./NextButton";
+import TheatersDataService from '../services/Theaters';
 
 class SeatSelection extends React.Component {
-    handleSelectSeat(e) {
-        if (e.currentTarget.classList.contains("seat") && !e.currentTarget.classList.contains("unavailable")) {
-            e.currentTarget.classList.toggle("selected");
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true,
+            curSeat: [],
+            seats: []
         }
     }
 
+    updateSelectedSeats = (seatPos) => {
+        if (!this.state.seats.includes(seatPos)) {
+            this.setState({
+                seats: this.state.seats.concat(seatPos)
+            });
+        } else {
+            let index = this.state.seats.indexOf(seatPos);
+            if (index !== -1) {
+                this.state.seats.splice(seatPos, 1);
+            }
+        }
+
+        console.log(this.state.seats)
+    }
+
+    handleSelectedSeat = (e) => {
+        e.preventDefault();
+
+        let seatPos = e.target.id;
+
+        if (e.currentTarget.classList.contains("seat") && !e.currentTarget.classList.contains("unavailable")) {
+            e.currentTarget.classList.toggle("selected");
+
+            this.updateSelectedSeats(seatPos);
+        }
+    }
+
+    componentDidMount() {
+        TheatersDataService.getSeatAvailablity(this.props.match.params.roomNumber)
+            .then(res => {
+                this.setState({
+                    loading: false,
+                    curSeat: res.data.room.seats
+                }, () => {
+                    console.log(this.state.curSeat)
+                })
+            }).catch(e => console.log(e.target.id));
+    }
+
     render() {
+        const seatMap = this.state.curSeat.map(seat => {
+            return (
+                <SeatTile
+                    isAvailable={seat.isAvailable}
+                    isHandicapped={seat.isHandicapped}
+                    position={seat.position}
+                    selectFunc={this.handleSelectedSeat}
+                    setter={this.props.setters.setSeats}
+                />
+            )
+
+        })
         return (
             <div className="auditorium">
+                {this.state.loading ?
+                    <p>Loading</p> :
+                    <>
+                        <div className="seat-tile-container">
+                            <div className="screen"></div>
 
-                <div className="seat-tile-container">
+                            <div className="layout">
+                                <div className="seat-row">
+                                    {seatMap}
+                                </div>
+                            </div>
 
-                    <div className="screen"></div>
+                            <p className="auditori-msg">Back of Theater</p>
 
-                    <div className="layout">
-                        <div className="seat-row">
-                            <div className={"seat"} onClick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
+                            <ul className="seats-legend" >
+                                <li>
+                                    <div className="seat available"></div>
+                                    <span>Available</span>
+                                </li>
+                                <li>
+                                    <div className="seat selected"></div>
+                                    <span>Selected</span>
+                                </li>
+                                <li>
+                                    <div className="seat handicapped"></div>
+                                    <span>Wheelchair Accessible</span>
+                                </li>
+                                <li>
+                                    <div className="seat unavailable"></div>
+                                    <span>Unavailable</span>
+                                </li>
+                            </ul>
                         </div>
 
-                        <div className="seat-row">
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                        </div>
-
-                        <div className="seat-row">
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                        </div>
-
-                        <div className="seat-row">
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                        </div>
-
-                        <div className="seat-row">
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                        </div>
-
-                        <div className="seat-row">
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                        </div>
-
-                        <div className="seat-row">
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                        </div>
-
-                        <div className="seat-row">
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                        </div>
-
-                        <div className="seat-row">
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                            <div className={"seat"} onclick={this.handleSelectSeat}></div>
-                        </div>
-
-                        <p className="auditori-msg">Back of Theater</p>
-
-                        <ul class="seats-legend">
-                            <li>
-                                <div class="seat"></div>
-                                <span>Available</span>
-                            </li>
-                            <li>
-                                <div class="seat selected"></div>
-                                <span>Selected</span>
-                            </li>
-                            <li>
-                                <div class="seat wheel"></div>
-                                <span>Wheelchair Accessible</span>
-                            </li>
-                            <li>
-                                <div class="seat unavailable"></div>
-                                <span>Unavailable</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <BackButton />
-                <NextButton />
+                        <BackButton />
+                        <NextButton />
+                    </>
+                }
             </div>
         )
     }
--- client/src/Components/SeatTile.js
+++ client/src/Components/SeatTile.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 class SeatTile extends React.Component {
+    constructor(props, context){
+        super(props, context);
+
+        this.handleSelectedSeat = this.handleSelectedSeat.bind(this);
+    }
+
+    handleSelectedSeat(e) {
+        let n = this.props.selectFunc(e);
+        this.props.setter(n);
+    }
+
     render() {
         return (
-            <div
-                className="seatTile"
-            >
+            <div className="seatTile">
+                <div
+                    className={"seat"}
+                    onClick={this.handleSelectedSeat}
+                    id={this.props.position}
+                >
+                </div>
             </div>
         )
     }
