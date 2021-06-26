1afab25f8e0a3a378ded1da28ec2e890196fb1fa
Z <zyang.0085@gmail.com>
2021-06-25 23:03:50 -0400

    Added documentation for new function
    #23

--- client/src/services/Theaters.js
+++ client/src/services/Theaters.js
@@ -14,6 +14,19 @@ class TheatersDataService {
         return http.get(`/theater/getRoomByMovieName?name=${movieName}`)
     }
 
+    /**
+     * Invert the availability of the given seat in the given room.
+     *
+     * Sample usage:
+     * {@code
+     *      TheatersDataService.setSeatAvailability(1, "F5")
+     *          .then(...)
+     *          .catch(...);
+     * }
+     *
+     * @param roomID    (Number) the room number
+     * @param position  (String) position of seat
+     */
     async setSeatAvailability(roomID, position) {
         return http.put(`/theater/seats/${roomID}`, {position: position});
     }
