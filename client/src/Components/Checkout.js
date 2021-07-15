4b6ff2931d4a3520d27868e167c3e9afce0a380d
Martin Cheung <cheungt1@wit.edu>
2021-07-15 16:56:18 -0400

    Initial setup

---
+++ client/src/Components/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SeatTile from './SeatTile'
+import BackButton from "./BackButton";
+import NextButton from "./NextButton";
+
+class Checkout extends React.Component {
+    render() {
+        
+    }
+}
+
+export default Checkout;
