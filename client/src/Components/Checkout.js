b83cfdb19abfdf049475391992636a2ddab5ece6
Martin Cheung <cheungt1@wit.edu>
2021-07-15 17:49:08 -0400

    added basic layout

--- client/src/Components/Checkout.js
+++ client/src/Components/Checkout.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import SeatTile from './SeatTile'
-import BackButton from "./BackButton";
-import NextButton from "./NextButton";
 
 class Checkout extends React.Component {
     render() {
-        
+        return (
+            <div className="check-page">
+                <div className="cart">
+                    <h2>Order Total</h2>
+                </div>
+                <div className="person-info">
+                    <div className="row" id="personal">
+                        <h2>Your Information</h2>
+                        <label for="email">Email</label>
+                        <input type="text" id="email" name="email" placeholder="johnsmith@email.com"/>
+                    </div>
+                    <div className="row" id="credit">
+
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
