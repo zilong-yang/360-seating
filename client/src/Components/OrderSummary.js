7b1ffd11f7efe07f33479f9c4e88ca8e1ea91235
Z <zyang.0085@gmail.com>
2021-07-18 20:16:25 -0400

    Added simple styles

--- client/src/Components/OrderSummary.js
+++ client/src/Components/OrderSummary.js
@@ -12,19 +12,32 @@ const OrderSummary = (props) => {
 
     return (
         <>
-            <h1>Order Summary Page</h1>
+            <div className='summary-container'>
+                <div className='page-name-container'>
+                    <h1 id='page-name'>Order Summary</h1>
+                    <hr width='90%' />
+                </div>
 
-            <div className={'summary'}>
-                <p>Movie: {order.movieName}</p>
-                <p>Time: {order.movieTime}</p>
-                <p>Room {order.roomNumber}</p>
-                <p>{order.numAdults + order.numChildren + order.numSeniors} Tickets</p>
-                <p>Seats: {order.seats.toString()}</p>
-                <p>Order Total: {order.total}</p>
+                <div className='summary'>
+                    <p>Movie: {order.movieName}</p>
+                    <p>Time: {order.movieTime}</p>
+                    <p>Room {order.roomNumber}</p>
+                    <p>{order.numAdults + order.numChildren + order.numSeniors} Tickets</p>
+                    <p>Seats: {order.seats}</p>
+                    <p>Order Total: {order.total}</p>
+
+                    <br />
+
+                    {/*TODO: fill in with information from checkout page*/}
+                    <p>Name: {"John Doe"}</p>
+                    <p>Email: {"john.doe@gmail.com"}</p>
+                    <p>Phone: {"123-456-7890"}</p>
+                    <p>Card: {"•••• •••• •••• 1234"}</p>
+                </div>
             </div>
 
-            <BackButton />
-            <NextButton name={'Confirm'} />
+                <BackButton />
+                <NextButton name={'Confirm'} />
         </>
     )
 }
